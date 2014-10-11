e3e439c60a053212fb58bc3b045dbbfde7f21004
flyher <dong3580@gmail.com>
2014-10-12 01:12:37 +0800

    change duoshuo id

--- js/common.js
+++ js/common.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
 	url=url.split("/");
 	var artid=url[url.length-1];
 	var title=artid.replace(".html","");
-	$("#divmsg").html("<div class=\"ds-thread\" data-thread-key=\""+artid+"\" data-title=\""+title+"\" data-url=\"http:\/\/flyher.github.io\/shumeipai\/md\/"+artid+"\"></div>");
+	//$("#divmsg").html("<div class=\"ds-thread\" data-thread-key=\""+artid+"\" data-title=\""+title+"\" data-url=\"http:\/\/flyher.github.io\/shumeipai\/md\/"+artid+"\"></div>");
+	$("#divmsg").html("<div class=\"ds-thread\" data-thread-key=\""+"index"+"\" data-title=\""+title+"\" data-url=\"http:\/\/flyher.github.io\/shumeipai\/md\/"+artid+"\"></div>");
 });
 <!-- ¶àËµÆÀÂÛ¿ò end -->
 var duoshuoQuery = {short_name:"smp"};
